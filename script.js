4f516dcea0875552a00ac1379c99dd38fb56d13e
February191999 <96108108+February191999@users.noreply.github.com>
2024-05-03 23:17:32 +0700

    Create variables to store first number, second number, and operation

--- script.js
+++ script.js
@@ -6,11 +6,18 @@ function subtractNumbers(firstNum, secondNum) {
     return firstNum - secondNum;
 }
 
-function multiplyNumber(firstNum, secondNum) {
+function multiplyNumbers(firstNum, secondNum) {
     return firstNum * secondNum;
 }
 
-function divideNumber(firstNum, secondNum) {
+function divideNumbers(firstNum, secondNum) {
     return firstNum / secondNum;
 }
 
+let firstNumber;
+let secondNumber;
+let operator;
+
+function calculator(firstNum, secondNum, operation) {
+
+}
