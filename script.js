27a7b778c1c858184dfb1fdb40a210f2bdc71720
February191999 <96108108+February191999@users.noreply.github.com>
2024-05-03 23:27:15 +0700

    Create operate function to add, subtract, multiply, or divide based on switch case statements of operator

--- script.js
+++ script.js
@@ -18,6 +18,19 @@ let firstNumber;
 let secondNumber;
 let operator;
 
-function calculator(firstNum, secondNum, operation) {
-
+function operate(firstNum, secondNum, operator) {
+    switch (operator) {
+        case ("add"): 
+            addNumbers(firstNum, secondNum);
+            break;
+        case ("subtract"):
+            subtractNumbers(firstNum, secondNum);
+            break;
+        case ("multiply"):
+            multiplyNumbers(firstNum, secondNum);
+            break;
+        case ("divide"):
+            divideNumbers(firstNum, secondNum);
+            break;
+    }   
 }
