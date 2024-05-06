af22450537c8204af84a38678a50d00e19dca69c
February191999 <96108108+February191999@users.noreply.github.com>
2024-05-07 01:18:35 +0700

    Use forEach function to iterate through numbers and add event listener to each one to print a number according to value if clicked

--- script.js
+++ script.js
@@ -17,17 +17,27 @@ function divideNumbers(firstNum, secondNum) {
 let firstNumber;
 let secondNumber;
 let operator;
-let displayValue;
+let displayValue = '';
 
-const numbers = document.querySelector(".number");
+const zero = document.querySelector(".zero");
+const one = document.querySelector(".one");
+const two = document.querySelector(".two");
+const three = document.querySelector(".three");
+const four = document.querySelector(".four");
+const five = document.querySelector(".five");
+const six = document.querySelector(".six");
+const seven = document.querySelector(".seven");
+const eight = document.querySelector(".eight");
+const nine = document.querySelector(".nine");
+const numbers = document.querySelectorAll(".number");
+const numList = [zero, one, two, three, four, five, six, seven, eight, nine];
 const displayDiv = document.querySelector(".display-div");
 const displayPara = document.createElement("p");
 
-numbers.addEventListener("click", () => {
-    alert(numbers.value);
-})
-
-
+numbers.forEach(num => num.addEventListener("click", () => {
+    displayPara.textContent += num.value;
+    displayDiv.appendChild(displayPara);
+}))
 
 function operate(firstNum, secondNum, operator) {
     switch (operator) {
